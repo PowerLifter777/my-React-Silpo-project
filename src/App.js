94f758e747339995ef6660fb9127a785812fe8c1
Vitalii Pasechnyk <vitalii.pasechnik@gmail.com>
2022-07-31 02:48:52 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -7,13 +7,41 @@ function App() {
   const [isHeaderVisible, setIsHeaderVisible] = useState(true);
 
 
-  const hide = (e) => {
+  // const hide = (e) => {
+  //   setIsHeaderVisible(false);
+  //   setTimeout(() => setIsHeaderVisible(true), 1500);
+  //   console.log(isHeaderVisible);
+  // }
+
+  let flag;
+  let visible1;
+  let visible2;
+  const hide = () => {
     setIsHeaderVisible(false);
-    setTimeout(() => setIsHeaderVisible(true), 1500);
-    console.log(isHeaderVisible);
+    if (!flag) {
+      visible1 = setTimeout(display, 800);
+      clear();
+      flag = true;
+    } if (flag) {
+      visible2 = setTimeout(display, 800);
+      clear();
+      flag = false;
+    }
   }
 
+  function clear() {
+    if (!flag) {
+      clearTimeout(visible2);
+    } if (flag) {
+      clearTimeout(visible1);
+    }
+  }
 
+  function display() {
+    setIsHeaderVisible(true)
+    console.log(flag);
+    flag = false;
+  }
 
   return (
     <div
