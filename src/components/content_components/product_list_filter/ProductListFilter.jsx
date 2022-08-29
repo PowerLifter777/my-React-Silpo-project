9ac1b5b67951deed013116b3d1bace135e343553
Vitalii Pasechnyk <vitalii.pasechnik@gmail.com>
2022-08-29 22:46:59 +0300

    commit

--- src/components/content_components/product_list_filter/ProductListFilter.jsx
+++ src/components/content_components/product_list_filter/ProductListFilter.jsx
@@ -20,18 +20,19 @@ const ProductListFilter = () => {
                         </svg> */}
                     </i>
                 </div>
-                <div className={`${classes.filter_wrapper_cover} ${classes.active}`}>
-                    {/* <ul class="checkbox-list-vertical">
-                        <li class="block-checkbox-item">
-                            <div class="block-checkbox-item_title">
-                                <div class="block-checkbox">
-                                </div>Піца</div>
-                            <div class="block-checkbox-item_count">29</div>
+                <div className={`${classes.filter_wrapper_cover} ${classes.filter_wrapper_cover_active}`}>
+                    <ul className={classes.checkbox_list_vertical}>
+                        <li className={`${classes.block_checkbox_item} ${classes.block_checkbox_item_active}`}>
+                            <div className={classes.block_checkbox_item_title}>
+                                <div className={classes.block_checkbox}></div>
+                                Піца
+                            </div>
+                            <div className={classes.block_checkbox_item_count}>29</div>
                         </li>
                     </ul>
                 </div>
             </article>
-            <article class="filter-wrapper">
+            {/* <article class="filter-wrapper">
                 <div class="filter-wrapper__title">«Сільпо» Resto<i class="icon icon-chevron-down">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 8" width="14" height="8">
                         <path d="M14 1.4 7 8 0 1.4 1.4 0 7 5.3 12.6 0z">
@@ -73,9 +74,9 @@ const ProductListFilter = () => {
                             <div class="rc-slider-handle rc-slider-handle-2" tabindex="0" role="slider" aria-valuemin="70" aria-valuemax="229" aria-valuenow="229" aria-disabled="false" style="left: 100%; transform: translateX(-50%);">
                             </div>
                         </div>
-                    </div> */}
+                    </div>
                 </div>
-            </article>
+            </article> */}
         </div>
     )
 }
